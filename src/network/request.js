7ee394e5f36d34cc078f84f4fdd1e92ec991ba6c
派大星的脚 <2250652265@qq.com>
2021-09-22 13:56:15 +0800

    '更改接口'

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   //创建axios实例
   const instance = axios.create({
-    baseURL:'http://152.136.185.210:7878/api/m5',
+    baseURL:'http://152.136.185.210:7878/api/hy66',
     timeout:5000
   })
 
@@ -25,3 +25,5 @@ export function request(config) {
   // 3.发送真正的网络请求
   return instance(config)
 }
+
+
